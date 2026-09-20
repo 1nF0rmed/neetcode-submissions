70ce504c9bf8284b1f0e08df3d0989d660e08079
Pradyumna Rahul <prkinformed@gmail.com>
2026-09-20 14:32:45 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.go
@@ -0,0 +1,23 @@
+func isAnagram(s string, t string) bool {
+	characterCount := make(map[byte]int)
+
+	if(len(s)!=len(t)) {
+		return false;
+	}
+
+	for i:=0;i<len(s);i++ {
+		characterCount[s[i]] = characterCount[s[i]]+1;
+	}
+
+	for j:=0;j<len(t);j++ {
+		characterCount[t[j]] = characterCount[t[j]]-1;
+	}
+
+	for _, count := range characterCount {
+		if count<0 || count>0 {
+			return false;
+		}
+	}
+
+	return true;
+}
